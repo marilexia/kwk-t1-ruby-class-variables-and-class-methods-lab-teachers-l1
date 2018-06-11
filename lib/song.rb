08cfe929b5badf978d0289615fcaa3c4147c2050
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-11 22:37:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,13 +3,13 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  
+
 
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @@count += 1
-    @@artists = []
+    @@artists << artist
     @@genres << genre
 
     @name = name
