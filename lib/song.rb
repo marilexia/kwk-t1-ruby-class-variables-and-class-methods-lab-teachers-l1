379c4bcc7b65ad763e8a6957539330d30e3bce31
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-11 22:16:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,17 +16,17 @@ class Song
   end
 
 
-  # def name
-  #   @name
-  # end
-  #
-  # def artist
-  #   @artist
-  # end
-  #
-  # def genre
-  #   @genre
-  # end
+  def name
+    @name
+  end
+  
+  def artist
+    @artist
+  end
+  
+  def genre
+    @genre
+  end
 
 
 end
